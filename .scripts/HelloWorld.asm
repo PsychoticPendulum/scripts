de225cc533dcbba585f81300e07274b3d231857d
PsychoticPendulum <80325726+PsychoticPendulum@users.noreply.github.com>
2021-05-20 09:08:28 +0200

    Add files via upload

---
+++ .scripts/HelloWorld.asm
@@ -0,0 +1,20 @@
+; Written in x86 Assembly
+
+global _start
+
+section .text:
+
+_start:
+	mov eax, 0x4
+	mov ebx, 1
+	mov ecx, msg
+	mov edx, msg_length
+	int 0x80
+
+	mov eax, 0x1
+	mov ebx, 0
+	int 0x80
+
+section .data:
+	msg: db "Hello World!", 0xA
+	msg_length: equ $-msg

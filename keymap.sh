afea1cfd801c29aa0ccfad24f2d02ae815fd5f08
PsychoticPendulum <lukas.mairock@gmail.com>
2022-07-06 16:53:29 +0200

    change keymap

--- keymap.sh
+++ keymap.sh
@@ -1,18 +1,13 @@
 #! /bin/bash
-# |-------------------------------------------------------------------------|
-# |  ____                 _     _      ____                        _        |
-# | |  _ \ ___ _   _  ___| |__ (_) ___|  _ \ ___ _ __   __ _ _   _(_)_ __   |
-# | | |_) / __| | | |/ __| '_ \| |/ __| |_) / _ \ '_ \ / _` | | | | | '_ \  |
-# | |  __/\__ \ |_| | (__| | | | | (__|  __/  __/ | | | (_| | |_| | | | | | |
-# | |_|   |___/\__, |\___|_| |_|_|\___|_|   \___|_| |_|\__, |\__,_|_|_| |_| |
-# |           |___/                                   |___/                 |
-# |-------------------------------------------------------------------------| 
 
-echo "Setting keyboard to DE ..."
-setxkbmap en_US
-echo "Setting CAPSLOCK to ESCAPE ..."
-xmodmap -e 'keycode 66 = Escape'
-echo "disabling CAPSLOCK ..."
-xmodmap -e 'clear lock'
-echo "Settings keyrate to 300, 50 ..."
-xset r rate 300 50
+CURRENT=$(setxkbmap -query | tail -n 1 | awk '{print $2}')
+
+echo Current is $CURRENT
+
+if [[ $CURRENT == "de" ]]; then
+	echo 'Setting to en_US'
+	setxkbmap en_US
+else
+	echo 'Setting to de'
+	setxkbmap de
+fi

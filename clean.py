57d94af8472036b82a320992dcafc0d1c70ab42c
PsychoticPendulum <lukas.mairock@gmail.com>
2022-07-15 14:18:31 +0200

    something with rainbows

---
+++ clean.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+
+import os
+
+user = os.popen('whoami').read().rstrip('\n')
+
+def DiskUsage():
+    return os.popen('df / | awk \'{print $3}\' | tail -n 1').read().rstrip('\n')
+
+def Clean():
+    os.system(f'paccache -r')
+    os.system(f'rm -Rfv /home/{user}/.cache/yay/*')
+
+if __name__ == '__main__':
+    before = DiskUsage()
+    Clean()
+    after = DiskUsage()
+    saved = (float(before) - float(after)) / 1000.0  
+    print(f'Cleaned up {saved} MiB')

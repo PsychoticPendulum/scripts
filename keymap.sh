3a7fcf39960f5a8e02714ec46c404ef006e61149
psychicpenguin <lukas.mairock@gmail.com>
2022-07-19 18:56:36 +0200

    from en_US to us

--- keymap.sh
+++ keymap.sh
@@ -5,8 +5,8 @@ CURRENT=$(setxkbmap -query | tail -n 1 | awk '{print $2}')
 echo Current is $CURRENT
 
 if [[ $CURRENT == "de" ]]; then
-	echo 'Setting to en_US'
-	setxkbmap en_US
+	echo 'Setting to us'
+	setxkbmap us 
 else
 	echo 'Setting to de'
 	setxkbmap de

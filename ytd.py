f83e1f14745479d6ef91b37d59ea1be8c99df994
luks <lukas.mairock@gmail.com>
2022-12-23 11:54:27 +0100

    multithreading

--- ytd.py
+++ ytd.py
@@ -1,20 +1,58 @@
 #! /usr/bin/env python3
 
-from Log import *
-from sys import argv
-from os import system
+import os
+import sys
+import threading
 
-if __name__ == '__main__':
-    if len(argv) < 2:
-        exit(1)
-    
+from PyTerm import *
+
+def Usage(status):
+    print("Usage: download.py <path to file>")
+    exit(status)
+
+def ParseFile(fileptr):
+    result = []
     try:
-        file = open(argv[1], 'r')
+        file = open(fileptr,"r+")
         for line in file.readlines():
-            line = line.rstrip('\n')
-            system(f'youtube-dl {line}')
+            result.append(line.rstrip('\n'))
         file.close()
-    except PermissionError:
-        Log(LVL.ERROR, f"Insufficient permissions to read: {UTIL.BOLD}{argv[1]}")                                      
     except FileNotFoundError:
-        Log(LVL.ERROR, f"File does not exist: {UTIL.BOLD}{argv[1]}")  
+        Log(LVL.ERROR, "File does not exist")
+        exit(1)
+    except PermissionError:
+        Log(LVL.ERROR, "Insufficient permissions to read file")
+        exit(1)
+    return result
+
+# This function was written by ChatGPT
+def SplitArray(arr,threads):
+    subarray_size = len(arr) // threads
+    subarrays = []
+    for i in range(threads):
+        start = i * subarray_size
+        end = (i + 1) * subarray_size
+        subarrays.append(arr[start:end])
+    return subarrays
+
+def Download(links):
+    for link in links:
+        os.system(f"youtube-dl '{link}'")
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1 or (sys.argv[1] == "-h" or sys.argv[1] == "--help"):
+        Usage(int(len(sys.argv)==1))
+
+    links = ParseFile(sys.argv[1])
+    thread_count = len(links) if len(links) < os.cpu_count() else os.cpu_count()
+    link_arrays = SplitArray(links,thread_count)
+   
+    Log(LVL.INFO, f"Creating {thread_count} threads")
+
+    threads = []
+    for link_array in link_arrays:
+        thread = threading.Thread(target=Download,args=(link_array,))
+        threads.append(thread)
+
+    for thread in threads:
+        thread.start()

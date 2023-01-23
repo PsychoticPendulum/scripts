9b4bcbc3e29036c80480b25638f93f61dc168de7
luks <lukas.mairock@gmail.com>
2023-01-23 12:09:34 +0100

    pacman cache and thumbnails

--- clean.py
+++ clean.py
@@ -8,8 +8,11 @@ def DiskUsage():
     return os.popen('df / | awk \'{print $3}\' | tail -n 1').read().rstrip('\n')
 
 def Clean():
+    os.system(f'rm -Rfv /home/{user}/.cache/thumbnails')
+    os.system(f'rm -Rfv /home/{user}/.cache/ranger')
     os.system(f'paccache -r')
     os.system(f'rm -Rfv /home/{user}/.cache/yay/*')
+    os.system(f'rm -Rfv /var/cache/pacman/pkg/*')
 
 if __name__ == '__main__':
     before = DiskUsage()

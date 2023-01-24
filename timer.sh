5c23fad074a3e0a779d945999f853dedf8c30f74
luks <lukas.mairock@gmail.com>
2023-01-24 10:55:58 +0100

    simple timer

---
+++ timer.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/bash
+
+time=$1
+if [[ -z $time ]]; then
+	echo -n "Time (in s): "
+	read time
+fi
+
+for i in $(eval echo "{1..$time}"); do
+	echo "Sleeping [$i/$time]"
+	sleep 1
+	if [[ $i != $time ]]; then
+		printf "\x1B[0m\x1B[1F\x1B[2K"
+	fi
+done

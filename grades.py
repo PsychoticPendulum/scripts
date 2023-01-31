286d12a00693db096a2de52c01de51bbe06d1f1e
luks <lukas.mairock@gmail.com>
2023-01-31 10:33:17 +0100

    calculate my total grade from list of all grades

---
+++ grades.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+
+import sys
+from PyTerm import *
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        Log(LVL.ERROR, "Not enough arguments")
+        exit(1)
+
+    grades = []
+    file = open(sys.argv[1],"r")
+    for line in file.readlines():
+        line = line.rstrip('\n')
+        try:
+            grades.append(int(line))
+        except:
+            pass
+
+    total = 0
+    for i in range(len(grades)):
+        total += grades[i]
+    print(f"{total} / {len(grades)+1} = {total/(len(grades)+1)}")

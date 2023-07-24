09ab00c392499a9be57b4f9eab8c3abc0b0086df
luks <lukas.mairock@gmail.com>
2023-07-24 19:33:21 +0200

    script to clone all repos

---
+++ allclone.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+
+import requests
+import os
+
+def clone_all_repositories(username):
+    api_url = f"https://api.github.com/users/{username}/repos"
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        repos = response.json()
+        for repo in repos:
+            repo_name = repo["name"]
+            repo_url = repo["clone_url"]
+            os.system(f"git clone {repo_url}")
+            print(f"Cloned repository {repo_name}")
+    else:
+        print(f"Failed to fetch repositories for {username}. Status code: {response.status_code}")
+
+if __name__ == "__main__":
+    github_username = "psychoticpendulum"
+    clone_all_repositories(github_username)

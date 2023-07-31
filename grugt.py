c31de473a558527f615ad7ed0615772c0bb9ce15
luks <lukas.mairock@gmail.com>
2023-07-31 13:01:42 +0200

    updates every git directory within a directory

---
+++ grugt.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+
+import os
+import sys
+from unilog import *
+
+def GetDirectories(root):
+    return [f.path for f in os.scandir(root) if f.is_dir()]
+
+def IsGitRepo(directory):
+    return os.path.isdir(f"{directory}/.git")
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        exit(1)
+    root = sys.argv[1]
+
+    directories = GetDirectories(root)
+    for directory in directories:
+        if IsGitRepo(directory):
+            Log(LVL.INFO, f"{directory}")
+            os.system(f"cd {directory}; git remote update; git pull; cd")

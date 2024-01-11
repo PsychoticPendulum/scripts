3d0ab3465102cfa90d3b7a3ed397cb57865b49bf
Lukas Mairock <lukas.mairock@gmail.com>
2024-01-11 09:58:19 +0100

    fixed bug if no session was selected

--- rdp.sh
+++ rdp.sh
@@ -1,4 +1,6 @@
 #! /usr/bin/bash
 
 SESSION=$(ls ~/.local/share/remmina/ | rofi -dmenu window -lines 0 -eh 1 -location 0 -width 644 -font "Source Code Pro Medium 10" -theme "~/.config/rofi/spotlight.rasi")
-remmina --connect=$HOME/.local/share/remmina/$SESSION
+if ! [ -z $SESSION ]; then
+	remmina --connect=$HOME/.local/share/remmina/$SESSION
+fi

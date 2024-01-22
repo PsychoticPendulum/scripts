d49e318037c03b44608827d34c559ba535d7ed23
luks <lukas.mairock@gmail.com>
2024-01-22 11:02:38 +0100

    removed stuff

--- theme.sh
+++ theme.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
 
 set_theme () {
-	cp ~/Developer/.scripts/themes/$1/i3.colorscheme ~/.config/i3/colorscheme
-	cp ~/Developer/.scripts/themes/$1/i3.style ~/.config/i3/style
-	cp ~/Developer/.scripts/themes/$1/polybar.colorscheme ~/.config/polybar/colorscheme
-	cp ~/Developer/.scripts/themes/$1/polybar.style ~/.config/polybar/style
-	cp ~/Developer/.scripts/themes/$1/kitty.conf ~/.config/kitty/kitty.conf
-	cp ~/Developer/.scripts/themes/$1/wallpaper.jpg ~/Wallpapers/wallpaper.jpg
+	cp ~/bin/themes/$1/i3.colorscheme ~/.config/i3/colorscheme
+	cp ~/bin/themes/$1/i3.style ~/.config/i3/style
+	cp ~/bin/themes/$1/polybar.colorscheme ~/.config/polybar/colorscheme
+	cp ~/bin/themes/$1/polybar.style ~/.config/polybar/style
+	cp ~/bin/themes/$1/kitty.conf ~/.config/kitty/kitty.conf
+	cp ~/bin/themes/$1/wallpaper.jpg ~/Wallpapers/wallpaper.jpg
 	feh --bg-fill ~/Wallpapers/wallpaper.jpg
 	echo $1 > ~/Developer/.scripts/themes/.last.log
 }
 
 if [[ -z "$1" ]]; then
-	theme=$(find ~/Developer/.scripts/themes/* -not -name '.*' -type d -exec basename {} \; | fzf)
+	theme=$(find ~/bin/themes/* -not -name '.*' -type d -exec basename {} \; | fzf)
 	[[ -n $theme ]] && set_theme $theme
 	killall polybar ; i3 restart ; clear
 fi

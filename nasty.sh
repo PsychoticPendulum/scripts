7ba7c91feaa13e4b1c3531c6e9305c9d16a85266
luks <lukas.mairock@gmail.com>
2024-02-07 12:27:58 +0100

    script to mount my network share

---
+++ nasty.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/bash
+
+read -s -p "Password: " password
+
+if [[ ! -z $password ]]; then 
+	sudo mount -t cifs //192.168.4.1/nasty /mnt/NAS -o username=luks,password=$password,uid=1000,gid=1000,file_mode=0664,dir_mode=0775
+	tree -L 1 /mnt/NAS
+fi
+

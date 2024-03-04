71df7c32506e9ac3d8174186d04074224798934c
luks <lukas.mairock@gmail.com>
2024-03-04 19:33:16 +0100

    renamed NAS to nas

--- nasty.sh
+++ nasty.sh
@@ -3,7 +3,7 @@
 read -s -p "Password: " password
 
 if [[ ! -z $password ]]; then 
-	sudo mount -t cifs //192.168.4.1/nasty /mnt/NAS -o username=luks,password=$password,uid=1000,gid=1000,file_mode=0664,dir_mode=0775
+	sudo mount -t cifs //192.168.4.1/nasty /mnt/nas -o username=luks,password=$password,uid=1000,gid=1000,file_mode=0664,dir_mode=0775
 	tree -L 1 /mnt/NAS
 fi
 

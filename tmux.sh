515cd50ed842761a142dc00d04b8b3649abf9e20
Lukas Mairock <lukas.mairock@gmail.com>
2024-08-19 07:56:49 +0200

    easy access to tmux sessions

--- tmux.sh
+++ tmux.sh
@@ -1,8 +1,10 @@
-#! /usr/bin/bash
+#!/bin/bash
 
-session_name=$(tmux list-sessions -F '#S' | fzf)
+session_name=$(tmux list-sessions -F '#S' | fzf --height=20% --reverse)
 
 if [ -n "$session_name" ]; then
     tmux attach-session -t "$session_name"
+else
+    echo "No session selected."
 fi
 

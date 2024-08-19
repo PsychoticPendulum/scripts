bb255c753a42af6e92f1819e708b82bf7c07547c
Lukas Mairock <lukas.mairock@gmail.com>
2024-08-19 14:06:29 +0200

    Debian instead of Artrix

--- neofetch.sh
+++ neofetch.sh
@@ -4,7 +4,7 @@ distro=$(cat /etc/issue | awk '{print $1}')
 
 if [[ $distro =~ "Void" ]]; then
 	neofetch
-elif [[ $distro =~ "Arch" ]] || [[ $distro =~ "Artix" ]]; then
+elif [[ $distro =~ "Arch" ]] || [[ $distro =~ "Debian" ]]; then
 	neofetch | lolcat -F 0.1 -a -s 5000 -S 174
 else
 	neofetch --config ~/.config/neofetch/config.conf.default
